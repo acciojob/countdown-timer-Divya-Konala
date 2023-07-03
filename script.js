828f92015c037e1977d58e119b0e588aa119c56c
Divya-Konala <87460968+Divya-Konala@users.noreply.github.com>
2023-07-03 11:36:10 +0530

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 // Your script here.
+const countDownDisplay=document.querySelector("#countDown");
+const endTimeDisplay=document.querySelector("#endTime");
+
+document.querySelector("button").addEventListener("click",()=>{
+    let countDownTime=document.querySelector("input").value;
+    let currDate=new Date();
+    let endTime=addMinutes(currDate,Number(countDownTime)).toLocaleTimeString();
+    countDownDisplay.textContent=currDate.toLocaleTimeString();
+    endTimeDisplay.textContent=endTime;
+    let displayTime = setInterval(()=>{
+        let currTime = new Date().toLocaleTimeString();
+        countDownDisplay.textContent=currTime;
+        if( currTime==endTime) clearInterval(displayTime);
+    },1000)
+})
+
+const addMinutes=(date,mins)=>{
+    date.setMinutes(date.getMinutes()+mins);
+    return date;
+}
+
+
